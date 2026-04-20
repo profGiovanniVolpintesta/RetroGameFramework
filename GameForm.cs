0b2e8e49b08951ec0d1f678f7d0e7d454dee644a
Giovanni Volpintesta <giovanni.volpintesta@itisriva.edu.it>
2026-04-20 20:01:13 +0200

    Main Window creation
    Main Window creation with matrix, pixel size, initialization values, and only 2 colors

---
+++ GameForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static RetroGameFramework.GameForm;
+
+namespace RetroGameFramework
+{
+    internal class GameForm : Form
+    {
+        public static class Initializer
+        {
+            public static int PixelSize = 10;
+            public static Color BackgroundColor = Color.Black;
+            public static Color ForegroundColor = Color.White;
+            public static string Title = "My Retro Game";
+        }
+
+        private int _pixelSize = Initializer.PixelSize;
+        public int PixelSize {
+            get { return _pixelSize; }
+            set
+            { 
+                _pixelSize = Math.Max(value, 0);
+                OnRefreshMatrix();
+            }
+        }
+
+        private bool[,] _matrix = null;
+        public bool[,] Matrix
+        {
+            get { return _matrix; }
+            set
+            {
+                _matrix = value;
+                OnRefreshMatrix();
+            }
+        }
+
+        public override Color BackColor
+        {
+            get { return base.BackColor; }
+            set
+            {
+                base.BackColor = value;
+                OnRefreshBrushes();
+            }
+        }
+
+        public override Color ForeColor
+        {
+            get { return base.ForeColor; }
+            set
+            {
+                base.ForeColor = value;
+                OnRefreshBrushes();
+            }
+        }
+
+        private Brush foreBrush = null;
+        private Brush backBrush = null;
+
+        public GameForm(bool[,] matrix) : base()
+        {
+            _matrix = matrix;
+            base.BackColor = Initializer.BackgroundColor;
+            base.ForeColor = Initializer.ForegroundColor;
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.Text = Initializer.Title;
+            OnRefreshMatrix();
+            OnRefreshBrushes();
+        }
+
+        private void OnRefreshMatrix()
+        {
+            this.ClientSize = new Size(Matrix.GetLength(0) * PixelSize, Matrix.GetLength(1) * PixelSize);
+        }
+
+        private void OnRefreshBrushes()
+        {
+            foreBrush = new SolidBrush(ForeColor);
+            backBrush = new SolidBrush(BackColor);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            
+            for (int x = 0; x < Matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < Matrix.GetLength(1); y++)
+                {
+                    e.Graphics.FillRectangle(Matrix[x, y] ? foreBrush : backBrush, x * PixelSize, y * PixelSize, PixelSize, PixelSize);
+                }
+            }            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RetroGameFramework
 {
@@ -10,6 +13,20 @@ namespace RetroGameFramework
     {
         static void Main(string[] args)
         {
+            //bool[,] matrix = new bool[64, 48];
+            bool[,] matrix = new bool[30, 30];
+
+            matrix[matrix.GetLength(0) / 2 - 1, matrix.GetLength(1) / 2 - 1] = true;
+            matrix[matrix.GetLength(0) / 2, matrix.GetLength(1) / 2 - 1] = true;
+            matrix[matrix.GetLength(0) / 2 - 1, matrix.GetLength(1) / 2] = true;
+            matrix[matrix.GetLength(0) / 2, matrix.GetLength(1) / 2] = true;
+
+            matrix[0, 0] = true;
+            matrix[matrix.GetLength(0) - 1, 0] = true;
+            matrix[0, matrix.GetLength(1) - 1] = true;
+            //matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1] = true;
+
+            Application.Run(new GameForm(matrix));
         }
     }
 }
