b09bb898dada80e6d293e1867e1c45a85575de3f
Giovanni Volpintesta <giovanni.volpintesta@itisriva.edu.it>
2026-04-21 04:47:04 +0200

    Input handling
    Input handling

--- BaseGameLogic.cs
+++ BaseGameLogic.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace RetroGameFramework
 {
@@ -7,7 +9,16 @@ namespace RetroGameFramework
     {
         private GameConfig _gameConfig;
         public GameConfig GameConfig { get { return _gameConfig; } }
-        
+
+        private List<Keys> _pressedKeys;
+        private List<Keys> _releasedKeys;
+        private List<Keys> _prevFramePressedKeys;
+        bool enabledInput = false;
+
+        private bool _paused = false;
+        public bool IsPaused() { return _paused; }
+        public void SetPaused (bool paused) { _paused = paused; }
+
         // CREATE GAME MATRIX
         static void Main(string[] args)
         {
@@ -22,7 +33,7 @@ namespace RetroGameFramework
 
             bool continueGame = true;
 
-            GameLogic.StartGame(PixelsMatrix);
+            GameLogic.StartGame(gameForm, PixelsMatrix);
             GameLogic.LoopGame(PixelsMatrix);
             gameForm.Invalidate();
             gameForm.Update();
@@ -41,11 +52,17 @@ namespace RetroGameFramework
                         gameForm.Invalidate();
                         gameForm.Update();
                     }));
-                    GameLogic.LoopGame(PixelsMatrix);
+
+                    GameLogic.CheckKeyboardEvents();
+
+                    if (!GameLogic.IsPaused())
+                    {
+                        GameLogic.LoopGame(PixelsMatrix);
+                    }
                 }
                 else
                 {
-                    GameLogic.EndGame();
+                    GameLogic.EndGame(gameForm);
                     gameTimer.Stop();
                 }
             };
@@ -57,6 +74,9 @@ namespace RetroGameFramework
         public BaseGameLogic(GameConfig GameConfig)
         {
             _gameConfig = GameConfig;
+            _pressedKeys = new List<Keys>();
+            _releasedKeys = new List<Keys>();
+            _prevFramePressedKeys = new List<Keys>();
         }
 
         private void InitGameConfig(GameConfig GameConfig)
@@ -66,8 +86,9 @@ namespace RetroGameFramework
         }
         protected virtual void OnInitGameConfig(GameConfig GameConfig) { }
 
-        private void StartGame(int[,] pixels)
+        private void StartGame(Form gameForm, int[,] pixels)
         {
+            EnableInput(gameForm);
             OnStartGame(pixels);
         }
         protected virtual void OnStartGame(int[,] pixels) { }
@@ -83,11 +104,140 @@ namespace RetroGameFramework
         protected virtual void OnLoopGame() { }
         protected virtual void OnDraw(int[,] pixels) { }
 
-        private void EndGame()
+        private void EndGame(Form gameForm)
         {
             OnEndGame();
+            DisableInput(gameForm);
         }
         protected virtual void OnEndGame() { }
 
+
+
+        private void EnableInput(Form gameForm)
+        {
+            enabledInput = true;
+            gameForm.KeyDown += OnKeyDownCallback;
+            gameForm.KeyUp += OnKeyUpCallback;
+        }
+
+        private void DisableInput(Form gameForm)
+        {
+            enabledInput = false;
+            gameForm.KeyDown -= OnKeyDownCallback;
+            gameForm.KeyUp -= OnKeyUpCallback;
+        }
+
+        private void OnKeyDownCallback(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (!_pressedKeys.Contains(e.KeyCode))
+            {
+                _pressedKeys.Add(e.KeyCode);
+            }
+        }
+
+        private void OnKeyUpCallback(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (!_releasedKeys.Contains(e.KeyCode))
+            {
+                _releasedKeys.Add(e.KeyCode);
+            }
+        }
+        private void CheckKeyboardEvents()
+        {
+            if (enabledInput)
+            {
+                // Create an array keysEvents of events, sorted firsst by priority, than by key.
+                Keys[] keysEvents;
+                List<Keys> keysToServe = new List<Keys>();
+
+                foreach (Keys key in _pressedKeys) { if (!keysToServe.Contains(key)) keysToServe.Add(key); }
+                foreach (Keys key in _releasedKeys) { if (!keysToServe.Contains(key)) keysToServe.Add(key); }
+                foreach (Keys key in _prevFramePressedKeys) { if (!keysToServe.Contains(key)) keysToServe.Add(key); }
+
+                keysEvents = keysToServe.ToArray();
+                Keys[] priorities = GameConfig.KeysPriority != null ? GameConfig.KeysPriority : new Keys[0];
+                Array.Sort(keysEvents, (Keys A, Keys B) =>
+                {
+                    int Apriority = Array.IndexOf(priorities, A);
+                    int Bpriority = Array.IndexOf(priorities, B);
+                    if (Apriority >= 0 && Bpriority >= 0) return Apriority - Bpriority;
+                    else if (Apriority >= 0 && Bpriority < 0) return -1000;
+                    else if (Bpriority >= 0 && Apriority < 0) return 1000;
+                    else return (int)A - (int)B;
+                });
+
+                // For each key, raise the proper events.
+                foreach (Keys key in keysEvents)
+                {
+                    bool raiseKeyDown = false, raiseKeyUp = false, raiseKeyPress = false;
+
+                    if (_prevFramePressedKeys.Contains(key))
+                    {
+                        raiseKeyPress = true;
+                    }
+
+                    if (!raiseKeyPress && _pressedKeys.Contains(key))
+                    {
+                        raiseKeyDown = true;
+                    }
+
+                    if (_releasedKeys.Contains(key))
+                    {
+                        raiseKeyUp = true;
+
+                        // Always invalidate pressed keys
+                        _pressedKeys.Remove(key);
+                        _prevFramePressedKeys.Remove(key);
+                        raiseKeyPress = false;
+                    }
+
+                    if (raiseKeyDown && raiseKeyUp) // check the events raise condition and decide what to do
+                    {
+                        switch (GameConfig.PressReleaseRaceConditionPolicy)
+                        {
+                            case GameConfig.PressReleaseRaceConditionRule.CallNone:
+                                raiseKeyDown = false;
+                                raiseKeyUp = false;
+                                break;
+                            case GameConfig.PressReleaseRaceConditionRule.PressWins:
+                                raiseKeyUp = false;
+                                break;
+                            case GameConfig.PressReleaseRaceConditionRule.ReleaseWins:
+                                raiseKeyDown = false;
+                                break;
+                        }
+                    }
+
+                    if (raiseKeyDown)
+                    {
+                        OnKeyDown(key);
+                    }
+
+                    if (raiseKeyUp)
+                    {
+                        OnKeyUp(key);
+                    }
+
+                    if (raiseKeyPress)
+                    {
+                        OnKeyPress(key);
+                    }
+                }
+            }
+
+            _prevFramePressedKeys.Clear();
+            _prevFramePressedKeys.AddRange(_pressedKeys);
+            _pressedKeys.Clear();
+            _releasedKeys.Clear();
+        }
+
+        // Called the first frame a key is pressed, and not called anymore unless the key is released
+        protected virtual void OnKeyDown(Keys KeyCode) { }
+
+        // Called if a key has been released (even in the same frame it has been released)
+        protected virtual void OnKeyUp(Keys KeyCode) { }
+
+        // Called during the frame a key is pressed and in all the following frames until it's released (excluding the frame it's released)
+        protected virtual void OnKeyPress(Keys KeyCode) { }
     }
 }
--- GameConfig.cs
+++ GameConfig.cs
@@ -4,11 +4,20 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RetroGameFramework
 {
     internal class GameConfig
     {
+        public enum PressReleaseRaceConditionRule
+        {
+            PressWins,
+            ReleaseWins,
+            CallBoth,
+            CallNone
+        }
+
         public int PixelsMatrixWidth = 50;
         public int PixelsMatrixHeight = 50;
         public int PixelSize = 10;
@@ -20,5 +29,13 @@ namespace RetroGameFramework
         public Color[] AdditionalColors = new Color[0];
         public Color InvalidColor = Color.Magenta;
         public string Title = "My Retro Game";
+
+        // Sorted array of key priorities.
+        // KeyCodes found earlier in this array are served before others found later.
+        // The input events of the same key are served in the following order: KeyDown, KeyUp, KeyPressed
+        public Keys[] KeysPriority = null;
+
+        // What happens if a key if pressed and released in the same frame?
+        public PressReleaseRaceConditionRule PressReleaseRaceConditionPolicy = PressReleaseRaceConditionRule.CallBoth;
     }
 }
